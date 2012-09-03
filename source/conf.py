16db6cacc645c8e9f509a73a52b123958a9b0e1d
John Roberts <jbr@pencoyd.com>
2012-09-03 14:47:31 -0700

    Small change

--- source/conf.py
+++ source/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'CloudFlare App Documentation'
+project = u'CloudFlare Apps documentation'
 copyright = u'2012, CloudFlare, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -183,8 +183,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'CloudFlareAppDocumentation.tex', u'CloudFlare App Documentation Documentation',
-   u'John Robers', 'manual'),
+  ('index', 'CloudFlareAppDocumentation.tex', u'CloudFlare App documentation',
+   u'John Roberts', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
--- source/index.rst
+++ source/index.rst
@@ -1,7 +1,7 @@
-CloudFlare App Marketplace
-==========================
+CloudFlare Apps
+===============
 
-http://www.cloudflare.com/apps
+https://www.cloudflare.com/apps
 
 Features
 ========
@@ -14,9 +14,11 @@ Documentation
    :maxdepth: 2
    
    gettingstarted
+   development-process
 
 
 Other resources
 ===============
 
 Sample app repo: https://github.com/cloudflare/cfapp_sample
+CloudFlareJS documentation and sandbox: `js.cloudflare.com<http://js.cloudflare.com>`_
