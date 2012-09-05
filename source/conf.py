184a5a295eed369ce7aa4118b2b2d01c2999aad9
John Roberts <jbr@pencoyd.com>
2012-09-05 12:43:31 -0700

    Changed config file
    html title

--- source/conf.py
+++ source/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -103,7 +103,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release>".
-#html_title = None
+html_title = "CloudFlare Apps developer documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -115,12 +115,12 @@ html_theme = 'default'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = ['../_static/favicon.ico']
+#html_favicon = ['favicon.ico']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -227,7 +227,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'CloudFlareAppDocumentation', u'CloudFlare App Documentation Documentation',
+  ('index', 'CloudFlareAppDocumentation', u'CloudFlare App Documentation',
    u'John Roberts', 'CloudFlareAppDocumentation', 'One line description of project.',
    'Miscellaneous'),
 ]
